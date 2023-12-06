d03229de577ccd1a9f71534960022c9388a59812
유창희 <wzmxm@naver.com>
2023-12-06 04:30:41 +0000

    업데이트

--- WEB-INF/src/dto/Product.java
+++ WEB-INF/src/dto/Product.java
@@ -14,6 +14,8 @@ public class Product implements Serializable { // 인터페이스 선언
 	private long unitsInStock; //재고수 
 	private String condition; 	//신상품 or 중고품 or 재생품
     private String filename;
+    private int quantity;	//장바구니에 담은 개수
+
     
 	public Product() {
 		super();
@@ -43,6 +45,15 @@ public class Product implements Serializable { // 인터페이스 선언
     public void setFilename(String filename) { // 파일 이름 설정
 	this.filename = filename;
     }
+    
+    public int getQuantity() {
+	return quantity;
+}
+
+public void setQuantity(int quantity) {
+	this.quantity = quantity;
+}
+
 
 
 	public void setProductId(String productId) {
